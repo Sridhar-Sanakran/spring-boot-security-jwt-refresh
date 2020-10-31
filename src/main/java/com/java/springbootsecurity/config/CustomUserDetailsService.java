f42924a314d9ec90b99136f360dbe60f8fa25df0
Sridhar-Sanakran <73736080+Sridhar-Sanakran@users.noreply.github.com>
2020-10-31 13:44:44 +0530

    Adding from local

---
+++ src/main/java/com/java/springbootsecurity/config/CustomUserDetailsService.java
@@ -0,0 +1,53 @@
+package com.java.springbootsecurity.config;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.java.springbootsecurity.model.DAOUser;
+import com.java.springbootsecurity.model.UserDTO;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+	
+	public static final Map<String,DAOUser> userRegistry = new HashMap();
+
+	@Autowired
+	private PasswordEncoder bcryptEncoder;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		List<SimpleGrantedAuthority> roles = null;
+		
+			
+		DAOUser user = userRegistry.get(username);
+		if (user != null) {
+			roles = Arrays.asList(new SimpleGrantedAuthority(user.getRole()));
+			return new User(user.getUsername(), user.getPassword(), roles);
+		}
+		throw new UsernameNotFoundException("User not found with the name " + username);	
+	}
+	
+	public DAOUser save(UserDTO user) {
+		DAOUser newUser = new DAOUser();
+		newUser.setUsername(user.getUsername());
+		newUser.setPassword(bcryptEncoder.encode(user.getPassword()));
+		newUser.setRole(user.getRole());
+		userRegistry.put(user.getUsername(),newUser);
+		System.out.println("User Saved!! "+newUser.getUsername());
+		return newUser;
+	}
+
+
+}
